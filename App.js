7762a3fe50dec1f86dc3ed7d76e6a6192860b8f2
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-02 20:44:07 +0200

    Trying to display a circle in skia

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Canvas, Circle } from "@shopify/react-native-skia";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Canvas style={{ flex: 1 }}>
+        <Circle cx={100} cy={100} r={128} color="lightblue" />
+      </Canvas>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

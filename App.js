fd3abdb58d4279a780953ea80f089c9704828920
Michael Yagudaev <michaelyagudaev@gmail.com>
2022-09-02 23:41:42 +0200

    Fix display

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Canvas, Circle } from "@shopify/react-native-skia";
+import { Box, Canvas, Circle } from "@shopify/react-native-skia";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -7,6 +7,11 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Canvas style={{ flex: 1 }}>
+        {/* <Box
+          color="lightblue"
+          box={{ width: 100, height: 100, x: 0, y: 0 }}
+          style="stroke"
+        /> */}
         <Circle cx={100} cy={100} r={128} color="lightblue" />
       </Canvas>
       <StatusBar style="auto" />
@@ -18,7 +23,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
